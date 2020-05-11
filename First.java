debdb3176d90f0dfdbd1ac7f2a16231facec96a3
RockyTheKiller <tukadiyar9@gmail.com>
2020-05-11 22:18:38 +0530

    Initial Commit

---
+++ First.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int m=a*b;
+		int p=a+b;
+		int d=a-b;
+		int devi=a/d;
+		System.out.println(p);
+		System.out.println(d);
+		System.out.println(m);
+		System.out.println(devi);
+	}
+
+}
